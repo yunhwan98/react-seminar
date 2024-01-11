14f23cd6bba4812519560fb76c3f44c3e6bbe7b9
박윤환 <park-yunhwan@bag-yunhwan-ui-MacBookPro-2.local>
2024-01-11 16:39:44 +0900

    style: 다크모드 스타일 변경

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,21 @@
 import { createTheme, responsiveFontSizes, Theme } from '@mui/material';
 
 import { DARK_MODE_THEME, LIGHT_MODE_THEME } from '../utils/constants';
+import { amber, deepOrange, grey, green, lightGreen } from '@mui/material/colors';
 
 export const getAppTheme = (mode: typeof LIGHT_MODE_THEME | typeof DARK_MODE_THEME): Theme => {
 	let theme = createTheme({
 		palette: {
 			mode,
+			...(mode === 'light' && {
+				// palette values for light mode
+				primary: lightGreen,
+				divider: lightGreen[200],
+				text: {
+					primary: grey[900],
+					secondary: grey[800],
+				},
+			}),
 		},
 	});
 	theme = responsiveFontSizes(theme);
