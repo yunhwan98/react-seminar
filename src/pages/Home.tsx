001710c8a32754e91b8616a5f2081d5c3af72517
박윤환 <park-yunhwan@bag-yunhwan-ui-MacBookPro-2.local>
2024-01-11 16:40:55 +0900

    feat: Home 페이지 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,44 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { Typography } from '@mui/material';
-import { useContext } from 'react';
+import { Grid, Typography } from '@mui/material';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Helmet } from 'react-helmet';
 
 import logo from '../logo.svg';
 
 import { AppContext } from '../contexts';
 import { APP_TITLE, PAGE_TITLE_HOME } from '../utils/constants';
+import useInput from '../hooks/useInput';
+import { parsing } from '../utils/crawl';
+import CourseItem from '../components/CourseItem/CourseItem';
 
+interface Review {
+	title: string;
+	content: string;
+	rating?: string;
+	id: number;
+	edit: boolean;
+}
+interface Course {
+	id: string;
+	title: string;
+	instructor?: string;
+	price?: string;
+	img: string;
+	rating?: string;
+	content?: string;
+}
 export const Home = () => {
 	const context = useContext(AppContext);
+	const [topList, setTopList] = useState<Course[]>([]);
+
+	useEffect(() => {
+		async function getData() {
+			let data = await parsing('fe', 'top');
+			setTopList(data);
+		}
+		getData();
+	}, []);
 
 	return (
 		<>
@@ -19,32 +47,16 @@ export const Home = () => {
 					{PAGE_TITLE_HOME} | {APP_TITLE}
 				</title>
 			</Helmet>
-			<Typography variant='h4'>{`Hello, ${context.user.name}`}</Typography>
-			<div
-				css={css`
-					text-align: center;
-					margin-top: 6rem;
-				`}
-			>
-				<img
-					css={css`
-						height: 40vmin;
-						pointer-events: none;
-						@media (prefers-reduced-motion: no-preference) {
-							animation: App-logo-spin infinite 15s linear;
-						}
-						@keyframes App-logo-spin {
-							from {
-								transform: rotate(0deg);
-							}
-							to {
-								transform: rotate(360deg);
-							}
-						}
-					`}
-					src={logo}
-					alt='logo'
-				/>
+
+			<div>
+				<h1>추천 강의 - 인프런</h1>
+				<Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 8, sm: 12, md: 16 }}>
+					{topList.map((course, idx) => (
+						<Grid item xs={4} sm={4} md={4} key={idx} mt={2}>
+							<CourseItem course={course} size='sm' />
+						</Grid>
+					))}
+				</Grid>
 			</div>
 		</>
 	);
